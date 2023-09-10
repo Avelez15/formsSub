94b285e02f5d04b88db5425648f968e12310b88e
Alejandro Velez <velez.alejandro4@gmail.com>
2023-09-09 21:52:26 -0400

    short subscription form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'forms-assignment';
+  @ViewChild('f') subscriptionForm:NgForm;
+  defaultSub = 'advanced';
+
+  subscription={
+    email: '',
+    password:''
+  }
+
+  submitted = false;
+
+  onSubmit(form:NgForm) {
+    this.subscription.email = this.subscriptionForm.value.userData.email;
+    this.subscription.password = this.subscriptionForm.value.userData.password;
+    this.submitted = true;
+
+    this.subscriptionForm.reset();
+  }
 }
